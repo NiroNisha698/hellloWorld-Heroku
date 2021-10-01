0fb9877c9c8f659cd349dd784a09cf2c52347a25
NiroshaSegar <niroshas@treinetic.com>
2021-10-02 02:25:59 +0530

    Initialize Git repository

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "Hello World!"
+
+
+# In[ ]:
+
+
+
+
